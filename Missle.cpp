e2ae9db15567324a1b42a28bde91ef7efec2d2a7
hydrater <liquid_assassin@hotmail.com>
2018-01-16 15:12:55 +0800

    Missle script

---
+++ Missle.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "Missle.h"
+#include "Vector3f.h"
+
+
+Missle::Missle()
+{
+}
+
+
+Missle::~Missle()
+{
+}
+
+static void FireMissle(Vector3f position, Vector3f dir)
+{
+
+}
